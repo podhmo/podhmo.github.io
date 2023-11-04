cf1fbce3dfd8e7f21e65d671ae8164f8b61512e4
podhmo <ababjam61+github@gmail.com>
2023-11-05 00:06:23 +0900

    feat: errorMessageのハンドリングなどを追加した

--- pwa/github-notifications/app.tsx
+++ pwa/github-notifications/app.tsx
@@ -1,5 +1,5 @@
 import { h, Fragment } from 'preact';
-import { useState } from 'preact/hooks'
+import { useCallback, useState } from 'preact/hooks'
 import type { ComponentChildren } from "preact";
 
 const state = {
@@ -91,17 +91,122 @@ export function InputForm({ onSubmit }: { onSubmit: todofixSubmitHandler }) {
     )
 }
 
-export function App() {
-    const handleSubmit = (ev) => {
-        ev.preventDefault()
-        console.log("ababa: ", JSON.stringify(state, null, null));
+export function OutputPanel({ input, version, errorMessage }) {
+    const style = { padding: "1rem" };
+    if (errorMessage !== "") {
+        return (<pre id="output" style={{ ...style, "background-color": "#fee" }}>{errorMessage}</pre>)
     }
+    return (<pre id="output" style={style}>version{version}: {JSON.stringify(state.input, null, null)}</pre>)
+}
+
+
+
+export function App() {
+    const [version, setversion] = useState(1);
+    const [errorMessage, seterrorMessage] = useState("");
+
+    const handleSubmit = useCallback(async (ev) => {
+        ev.preventDefault()
+        setversion((prev) => prev + 1) // TODO: cache
+        console.log("ababa: ", JSON.stringify(state, null, null));
+        try {
+        } catch (err) {
+            seterrorMessage(`err: ${err}\n\n${err.stack}`);
+            throw err;
+        }
+    }, [version])
     return (
         <>
             <h1 class="title">GitHub Notifications</h1>
             <InputForm onSubmit={handleSubmit}></InputForm>
             <p><a href="https://github.com/settings/tokens" target="_blank">please set PAT(personal access token)</a></p>
-            <pre id="output"></pre>
+            <OutputPanel input={state.input} version={version} errorMessage={errorMessage}></OutputPanel>
         </>
     );
+}
+
+// ----------------------------------------
+// helpers
+// ----------------------------------------
+async function fetchNotifications({ apikey, query, participating }) {
+    // https://docs.github.com/en/rest/activity/notifications?apiVersion=2022-11-28
+    const headers = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": `token ${apikey}`,
+        "X-GitHub-Api-Version": "2022-11-28"
+    };
+
+    let url = "https://api.github.com/notifications";
+    const qs = ["per_page=50", "all=false"];
+    if (query !== "") {
+        qs.push(`query=${encodeURIComponent(query)}`)
+    }
+    if (participating) {
+        qs.push(`participating=${participating}`)
+    }
+    if (qs.length > 0) {
+        url += "?" + qs.join("&")
+    }
+    const res = await fetch(url, { headers });
+    return res
+}
+
+
+export function filterResponseData({ rows, query, debug }) {
+    if (query !== "") { // 手抜きの query
+        // e.g. `is:unread org:encode`
+        query.split(/\s+/).forEach((q) => {
+            if (q === "") {
+                return
+            }
+
+            let [k, v] = q.split(":")
+            let isExclude = false;
+            if (v.startsWith("-")) {
+                isExclude = true;
+                v = v.slice(1)
+            }
+
+            if (k === "is") { // TODO: done,check-suite,commit,gist,release
+                if (v === "unread") {
+                    rows = rows.filter((d) => d.unread)
+                } else if (v === "read") {
+                    rows = rows.filter((d) => !d.unread)
+                } else if (v === "issue-or-pull-request") {
+                    rows = rows.filter((d) => d.subject.type === "Issue" || d.subject.type === "PullRequest")
+                } else if (v === "issue") {
+                    row = rows.filter((d) => d.subject.type === "Issue")
+                } else if (v === "pull-request") {
+                    rows = rows.filter((d) => d.subject.type === "PullRequest")
+                } else {
+                    throw new Error(`unknown query: ${q}`)
+                }
+            } else if (k === "org") {
+                rows = isExclude ? rows.filter((d) => d.repository.owner.login !== v) : rows.filter((d) => d.repository.owner.login === v)
+            } else if (k === "repo") {
+                rows = isExclude ? rows.filter((d) => d.repository.full_name !== v) : rows.filter((d) => d.repository.full_name === v)
+            } else if (k === "author") {
+                // not supported
+                //  rows = rows.filter((d) => d.subject.latest_comment_url.includes(v))
+            } else {
+                // TODO: reason
+                throw new Error(`unknown query: ${q}`)
+            }
+        })
+    }
+
+    if (!debug) {
+        rows = rows.map((d) => {
+            const id = d.id
+            const last_read_at = d.last_read_at;
+            const latest_comment_url = d.subject.latest_comment_url;
+            const title = d.subject.title;
+            const repository = d.repository.full_name;
+            const subjectType = d.subject.type;
+            const url = d.subject.url; // null if d.subject.type === "Discussion"
+            const owner = { name: d.repository.owner.login, avatar_url: d.repository.owner.avatar_url };
+            return { id, title, repository, url, subjectType, owner: owner, reason: d.reason, updated_at: d.updated_at, last_read_at, latest_comment_url };
+        })
+    }
+    return rows
 }
