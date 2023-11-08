5c7c62fda3fce0213549a28187be267e6be6e09e
podhmo <ababjam61+github@gmail.com>
2023-11-08 19:49:53 +0900

    feat: エラーになったときにraw error messageが自動で開くようにした

--- pwa/github-notifications/app.tsx
+++ pwa/github-notifications/app.tsx
@@ -18,7 +18,59 @@ const STATE = {
 const DEBUG = false;
 type todofixSubmitHandler = any
 
-export function InputForm({ onSubmit, loading }: { onSubmit: todofixSubmitHandler; loading: boolean }) {
+
+export function App() {
+    const [version, setversion] = useState(1);
+    const [rows, setrows] = useState([]); // TODO: rename to notifications[
+    const [loading, setloading] = useState(false);
+    const [errorMessage, seterrorMessage] = useState("");
+    const onError = useCallback((err: Error) => {
+        seterrorMessage(`err: ${err}\n\n${err.stack}`);
+    }, [])
+
+    const handleSubmit = useCallback(async (ev) => {
+        ev.preventDefault()
+        // console.log("submit ev:%o", ev);
+
+        setversion((prev) => prev + 1) // TODO: cache
+        // console.log("state: ", JSON.stringify(STATE, null, null));
+        try {
+            const state = STATE.input;
+            const query = state.query
+
+            setloading(() => true);
+            const res = await CLIENT.fetchNotifications({ query, apikey: STATE.apikey, participating: state.participating })
+            setloading(() => false);
+            if (res.status !== 200) {
+                seterrorMessage(`ng: ${res.status} ${res.statusText}: ${await res.text()}`);
+                return;
+            }
+
+            let rows = await res.json() as Array<any>; // xxx:
+            rows = filterResponseData({ rows, query, debug: state.debug });
+            setrows(rows);
+            seterrorMessage("");
+        } catch (err) {
+            onError(err);
+            throw err;
+        }
+    }, [version])
+
+
+    return (
+        <>
+            <h1 class="title">GitHub Notifications</h1>
+            <InputFormPanel onSubmit={handleSubmit} loading={loading}></InputFormPanel>
+            <p><a href="https://github.com/settings/tokens" target="_blank">please set PAT(personal access token)</a></p>
+
+            <RawOutputPanel input={STATE.input} rows={rows} version={version} errorMessage={errorMessage}></RawOutputPanel>
+
+            {rows && <CardListPanel rows={rows} onError={onError}></CardListPanel>}
+        </>
+    );
+}
+
+export function InputFormPanel({ onSubmit, loading }: { onSubmit: todofixSubmitHandler; loading: boolean }) {
     const [username, setusername] = useState(STATE.input.username);
     const [apikey, setapikey] = useState(STATE.apikey);
     const [query, setquery] = useState(STATE.input.query);
@@ -94,77 +146,44 @@ export function InputForm({ onSubmit, loading }: { onSubmit: todofixSubmitHandle
     )
 }
 
-export function OutputPanel({ input, rows, version, errorMessage }: { input?: any; rows?: Array<any>; version: number; errorMessage?: string }) {
+export function RawOutputPanel({ input, rows, version, errorMessage }: { input?: any; rows?: Array<any>; version: number; errorMessage?: string }) {
     const style = { padding: "1rem" };
     if (errorMessage !== "") {
-        return (<pre id="output" style={{ ...style, "background-color": "#fee" }}>{errorMessage}</pre>)
+        return (
+            <details open>
+                <summary> raw response</summary>
+                <pre id="output" style={{ ...style, "background-color": "#fee" }}>{errorMessage}</pre>
+            </details>
+        );
     }
+
     // return (<pre id="output" style={style}>version{version}: {JSON.stringify(input, null, null)}</pre>)
-    return (<pre id="output" style={style}>version{version}: {JSON.stringify(rows, null, 2)}</pre>)
+    return (
+        <details>
+            <summary> raw response</summary>
+            <pre id="output" style={style}>version{version}: {JSON.stringify(rows, null, 2)}</pre>
+        </details>
+    );
 }
 
-
-
-export function App() {
-    const [version, setversion] = useState(1);
-    const [rows, setrows] = useState([]); // TODO: rename to notifications[
-    const [loading, setloading] = useState(false);
-    const [errorMessage, seterrorMessage] = useState("");
-
-    const handleSubmit = useCallback(async (ev) => {
-        ev.preventDefault()
-        // console.log("submit ev:%o", ev);
-
-        setversion((prev) => prev + 1) // TODO: cache
-        // console.log("state: ", JSON.stringify(STATE, null, null));
-        try {
-            const state = STATE.input;
-            const query = state.query
-
-            setloading(() => true);
-            const res = await CLIENT.fetchNotifications({ query, apikey: STATE.apikey, participating: state.participating })
-            setloading(() => false);
-            if (res.status !== 200) {
-                seterrorMessage(`ng: ${res.status} ${res.statusText}: ${await res.text()}`);
-                return;
+function CardListPanel({ rows, onError, children }: { rows: Array<any>, onError: (err: Error) => void, children?: ComponentChildren }) {
+    try {
+        const props = rows.map((row) => {
+            const html_url = apiURLtohtmlURL(row.url);
+            const avatar_url = row.owner.avatar_url.includes("?") ? `${row.owner.avatar_url}&s=80` : `${row.owner.avatar_url}?s=80&v=4`;
+            const parts = html_url.split("/");
+            return {
+                "title": row.repository,
+                "typ": row.subjectType,
+                "link": { "href": html_url, "text": `#${parts[parts.length - 1]}`, "tab": true },
+                "message": { "text": row.title, author: { name: row.owner.name, url: avatar_url }, "cdate": row.updated_at }
             }
-
-            let rows = await res.json();
-            rows = filterResponseData({ rows, query, debug: state.debug });
-            setrows(rows);
-            seterrorMessage("");
-        } catch (err) {
-            seterrorMessage(`err: ${err}\n\n${err.stack}`);
-            throw err;
-        }
-    }, [version])
-
-
-    return (
-        <>
-            <h1 class="title">GitHub Notifications</h1>
-            <InputForm onSubmit={handleSubmit} loading={loading}></InputForm>
-            <p><a href="https://github.com/settings/tokens" target="_blank">please set PAT(personal access token)</a></p>
-
-            <details>
-                <summary> raw response</summary>
-                <OutputPanel input={STATE.input} rows={rows} version={version} errorMessage={errorMessage}></OutputPanel>
-            </details>
-
-            {rows && rows.map((row) => {
-                const html_url = apiURLtohtmlURL(row.url);
-                const avatar_url = row.owner.avatar_url.includes("?") ? `${row.owner.avatar_url}&s=80` : `${row.owner.avatar_url}?s=80&v=4`;
-                const parts = html_url.split("/");
-                const prop = {
-                    "title": row.repository,
-                    "typ": row.subjectType,
-                    "link": { "href": html_url, "text": `#${parts[parts.length - 1]}`, "tab": true },
-                    "message": { "text": row.title, author: { name: row.owner.name, url: avatar_url }, "cdate": row.updated_at }
-                };
-                return (<NotificationCard key={row.title} {...prop}></NotificationCard>);
-            })}
-        </>
-    );
+        });
+        return (<>{props.map((p) => <NotificationCard key={p.title} {...p}></NotificationCard>)}</>)
+    } catch (err) {
+        onError(err);
+        return
+    }
 }
 
 
