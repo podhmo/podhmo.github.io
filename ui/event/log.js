0fce47d3d51f7eb97dbc646e63adfa4de313340a
podhmo <ababjam61+github@gmail.com>
2024-10-04 01:18:10 +0900

    docs: doc string

--- ui/event/log.js
+++ ui/event/log.js
@@ -1,3 +1,9 @@
+// the log function which logs a message to the console and optionally appends it to a selector.
+// @param {string} msg - The message to log
+// @param {object} [options] - The options object
+// @param {string} [options.selector] - The selector to append the message to
+// @param {string} [options.tag] - The tag to use for the message element
+// @returns {void}
 export function log(msg, { selector = "", tag = "li" } = {}) {
     console.log(`%c${msg}`, "color: blue");
     if (selector !== "") {
@@ -7,6 +13,7 @@ export function log(msg, { selector = "", tag = "li" } = {}) {
     }
 }
 
+// partially apply the log function to bind it to a selector
 export function bind(selector) {
     return function (msg) {
         log(msg, { selector });
