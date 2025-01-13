760080060c74e7d06b0ed87c706fbcef52b63ece
podhmo <ababjam61+github@gmail.com>
2025-01-13 22:20:28 +0900

    chore: amazonのURLをシンプルにした

---
+++ pwa/mdcopy/simplify.mjs
@@ -0,0 +1,11 @@
+/** シンプルな表現のURLに変換する */
+export function symplifyURL(url) {
+    if (url.startsWith("https://www.amazon.") && url.includes("/dp/")) { // for Amazon product page
+        // e.g. https://www.amazon.com/{description}/dp/{asin}/...
+        const u = new URL(url);
+        const parts = u.pathname.split("/dp/");
+        const asin = parts[1].split("/")[0];
+        return `${u.origin}/dp/${asin}`;
+    }
+    return url;
+}
